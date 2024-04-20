e14c460ccb05f7e460c0aeece3be77e102db5e4a
Pritam Ghosh <44809987+meghoshpritam@users.noreply.github.com>
2024-04-20 14:31:47 +0530

    Update layout in builder.tsx and styles in onyx.tsx

--- apps/artboard/src/templates/onyx.tsx
+++ apps/artboard/src/templates/onyx.tsx
@@ -713,7 +713,7 @@ export const Onyx = ({ columns, isFirstPage = false }: TemplateProps) => {
   const [main, sidebar] = columns;
 
   return (
-    <div className="p-custom space-y-4">
+    <div className="p-custom space-y-6">
       {isFirstPage && <Header />}
 
       {main.map((section) => (
